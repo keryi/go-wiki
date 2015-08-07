b60fe440b740543a9a240ce5b8321df7162b025f
keryi <hti_32@yahoo.com>
2015-08-07 07:57:54 +0000

    add edit and save

--- wiki.go
+++ wiki.go
@@ -31,8 +31,31 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/edit/"):]
+  p, err := loadPage(title)
+  if err != nil {
+    p = &Page{Title: title}
+  }
+  fmt.Fprintf(w, "<h1>Editing %s</h1>" +
+    "<form action='/save/%s' method='POST'>" +
+    "<textarea name='body'>%s</textarea>" +
+    "<input type='submit' value='Save'/>" +
+    "</form>", p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/save/"):]
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  p.save()
+  http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
 func main() {
   http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHandler)
 	fmt.Printf("Server start at http://localhost:8080\n")
 	http.ListenAndServe(":8080", nil)
 }
