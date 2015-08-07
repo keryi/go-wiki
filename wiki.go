25e7fa7305f27510bea2ab683276207d8132f5f5
keryi <hti_32@yahoo.com>
2015-08-07 15:23:49 +0000

    fix regex

--- wiki.go
+++ wiki.go
@@ -16,8 +16,8 @@ type Page struct {
   Body []byte
 }
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-var validPath = regexp.MustCompile("^/(edit/save/view)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 var (
   addr = flag.Bool("addr", false, "find open address and print to final-port.txt")
 )
